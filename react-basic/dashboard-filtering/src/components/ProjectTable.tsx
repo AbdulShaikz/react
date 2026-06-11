0176d79a0007e0bc178723fe12d7589a8e08f307
AbdulShaikz <starabdul100@gmail.com>
2026-06-11 17:38:21 +0530

    feat: add sorting and filtering functionality in ProjectTable.tsx

--- react-basic/dashboard-filtering/src/components/ProjectTable.tsx
+++ react-basic/dashboard-filtering/src/components/ProjectTable.tsx
@@ -1,8 +1,27 @@
 import { useState } from "react";
 import { data } from "../utils/data";
+import { BiSort } from "react-icons/bi";
+import { AiOutlineDown } from "react-icons/ai";
+import { MdSort } from "react-icons/md";
+import { BsThreeDots } from "react-icons/bs";
 
 const ProjectTable = () => {
   const [projects, setProjects] = useState(data);
+  const [dropdownVisible, setDropdownVisible] = useState<boolean | null>(false);
+  const [filtersVisible, setFiltersVisible] = useState<boolean | null>(false);
+  const [sortConfig, setSortConfig] = useState<{key: string, direction: string} | null>(null);
+  const [filters, setFilters] = useState({
+    name: "",
+    country: "",
+    email: "",
+    project:"",
+    status:"",
+  });
+  const [statusDropdownVisible, setStatusDropdownVisible] = useState<number | null>(null);
+  const statusClasses: Record<string, string> = {
+    "Completed": "bg-green-500",
+    "In Progress": "bg-yellow-500",
+  };
 
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 5;
@@ -11,10 +30,139 @@ const ProjectTable = () => {
   const totalPages = Math.ceil(projects.length/itemsPerPage);
   const handlePageChange = (pageNumber: number) => setCurrentPage(pageNumber);
 
+  const handleSortOptionClick = (option: string)=>{
+    sortProjects(key);
+    setDropdownVisible(false);
+  };
+
+  const sortProjects = (option: string) => {
+    let sortedProjects = [...projects];
+    if(sortConfig && sortConfig.key===option && sortConfig.direction === 'ascending'){
+
+    }
+  }
+
+  const handleFilterChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setFilters({
+      ...filters,
+      [e.target.name]: e.target.value,
+    })
+  };
+
+  const handleStatusChange = (index:number, newStatus: string) => {
+    const updatedProjects = projects.map((project,idx) => (
+      idx==index? {
+        ...project,
+        status: newStatus,
+        progress: newStatus === "Completed"?"100%": project.progress,
+      } : project
+    ));
+    setProjects(updatedProjects);
+    setStatusDropdownVisible(null);
+  };
+
   return (
     <div className="p-4 w-[93%] ml-[5rem]">
       {/* Sorting */}
-
+      <div className="flex items-center mb-5">
+        <div className="relative">
+          <button
+            onClick={()=>{ setDropdownVisible(!dropdownVisible)}}
+            className="border border-gray-700 text-white flex items-center justify-center p-2 rounded"
+          >
+            <BiSort className="mr-[0.3rem]"/>
+            Sort
+            <AiOutlineDown className="ml-2"/>
+            {dropdownVisible && (
+              <div className="absolute top-full left-0 mt-2 rounded bg-gray-800 border border-gray-700 shadow-lg">
+                <button
+                  className="block px-4 py-2 text-white w-full hover:bg-gray-700"
+                   onClick={() => handleSortOptionClick("client")}
+                >
+                  Name
+                </button>
+                <button
+                  className="block px-4 py-2 text-white w-full hover:bg-gray-700"
+                   onClick={() => handleSortOptionClick("client")}
+                >
+                  Country
+                </button>
+                <button
+                  className="block px-4 py-2 text-white w-full hover:bg-gray-700"
+                   onClick={() => handleSortOptionClick("client")}
+                >
+                  Date
+                </button>
+              </div>
+            )}
+          </button>
+        </div>
+        {/* Filters */}
+        <div className="relative ml-4 w-full">
+          <button
+            onClick={() => setFiltersVisible(!filtersVisible)}
+            className="border border-gray-700 flex items-center justify-center text-white p-2 rounded"
+          >
+            <MdSort className="mr-[0.3rem]"/>
+            Filters
+            <AiOutlineDown className="ml-2"/>
+          </button>
+          {filtersVisible && (
+            <div className="absolute top-full left-0 mb-2 bg-gray-800 border border-gray-700 rounded shadow-lg">
+              <div className="mb-2">
+                <label className="block text-white"> Filter by Name:</label>
+                <input 
+                  type="text"
+                  name="name"
+                  value={filters.name}
+                  onChange={handleFilterChange}
+                  className="bg-gray-900 text-white rounded p-2 w-full"
+                />
+              </div>
+              <div className="mb-2">
+                <label className="block text-white"> Filter by Country:</label>
+                <input 
+                  type="text"
+                  name="country"
+                  value={filters.country}
+                  onChange={handleFilterChange}
+                  className="bg-gray-900 text-white rounded p-2 w-full"
+                />
+              </div>
+              <div className="mb-2">
+                <label className="block text-white"> Filter by Email:</label>
+                <input 
+                  type="text"
+                  name="email"
+                  value={filters.email}
+                  onChange={handleFilterChange}
+                  className="bg-gray-900 text-white rounded p-2 w-full"
+                />
+              </div>
+              <div className="mb-2">
+                <label className="block text-white"> Filter by Project:</label>
+                <input 
+                  type="text"
+                  name="project"
+                  value={filters.project}
+                  onChange={handleFilterChange}
+                  className="bg-gray-900 text-white rounded p-2 w-full"
+                />
+              </div>
+              <div className="mb-2">
+                <label className="block text-white"> Filter by Status:</label>
+                <input 
+                  type="text"
+                  name="status"
+                  value={filters.status}
+                  onChange={handleFilterChange}
+                  className="bg-gray-900 text-white rounded p-2 w-full"
+                />
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
       {/* Main Table */}
       <table className="min-w-full table-auto rounded border border-gray-700 text-white">
         <thead>
@@ -52,13 +200,36 @@ const ProjectTable = () => {
               </td>
               <td className="px-4 py-2 w-[10rem]">
                 <span
-                  className={`bg-${project.status==="Completed"?"green":"yellow"}-500 p-1 rounded`}
+                  className={`${statusClasses[project.status]} p-1 rounded`}
                 >
                   {project.status}
                 </span>
               </td>
               <td className="px-4 py-2">{project.date}</td>
-              <td className="px-4 py-2"></td>
+              <td className="px-4 py-2">
+                <div className="relative">
+                  <BsThreeDots
+                    className="cursor-pointer"
+                    onClick={() => setStatusDropdownVisible(index)}
+                  />
+                  {statusDropdownVisible && (
+                    <div className="absolute top-full right-0 mt-2 bg-gray-800 border border-gray-700 rounded shadow-lg">
+                      <button
+                        onClick={() => handleStatusChange(index, "In Progresss")}
+                        className="block px-4 py-2 text-white hover:bg-gray-700 w-full text-left"
+                      >
+                        In Progress
+                      </button>
+                      <button
+                        onClick={() => handleStatusChange(index, "Completed")}
+                        className="block px-4 py-2 text-white hover:bg-gray-700 w-full text-left"
+                      >
+                        Completed
+                      </button>
+                    </div>
+                  )}
+                </div>
+              </td>
             </tr>
           ))}
         </tbody>
